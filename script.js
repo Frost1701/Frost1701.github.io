41974006ab1c027918d54a881d72c7ee936fbfc3
Frost1701 <jackh1701@hotmail.com>
2026-05-19 00:24:03 +1000

    update javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+const toggles = document.getElementsByClassName("toggle");
+
+for (let i = 0; i < toggles.length; i++) {
+  toggles[i].onclick = () => {
+    const panel = toggles[i].nextElementSibling;
+    // if it has height, close it. otherwise open it to its natural size
+    if (panel.style.height && panel.style.height !== "0px") {
+      panel.style.height = "0px";
+    } else {
+      panel.style.height = panel.scrollHeight + "px";
+    }
+  };
+}
